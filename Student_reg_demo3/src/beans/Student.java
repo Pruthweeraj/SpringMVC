36c2034b6b7ed9e6c64d1f375bd920fe922b4a8d
pr <infinite>
2019-02-12 18:46:14 +0530

    SpringMvc

---
+++ Student_reg_demo3/src/beans/Student.java
@@ -0,0 +1,63 @@
+package beans;
+
+import java.util.Date;
+import java.util.List;
+
+public class Student {
+
+	String studentName;
+	String studentRollNumber;
+	long studentMobile;
+	Date studentDOB;
+	List<String> studentSkills;
+	Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getStudentRollNumber() {
+		return studentRollNumber;
+	}
+
+	public void setStudentRollNumber(String studentRollNumber) {
+		this.studentRollNumber = studentRollNumber;
+	}
+
+	public long getStudentMobile() {
+		return studentMobile;
+	}
+
+	public void setStudentMobile(long studentMobile) {
+		this.studentMobile = studentMobile;
+	}
+
+	public Date getStudentDOB() {
+		return studentDOB;
+	}
+
+	public void setStudentDOB(Date studentDOB) {
+		this.studentDOB = studentDOB;
+	}
+
+	public List<String> getStudentSkills() {
+		return studentSkills;
+	}
+
+	public void setStudentSkills(List<String> studentSkills) {
+		this.studentSkills = studentSkills;
+	}
+
+}
---
+++ Student_reg_demo3/src/controllers/RegistrationController.java
@@ -0,0 +1,56 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import beans.Student;
+
+@Controller
+public class RegistrationController {
+
+	@ModelAttribute
+	public void addCommonObject(Model model) {
+		model.addAttribute("headMessage", "PR COLLEGE OF ENGINEERING,ODISHA");
+
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder webDataBinder){
+		
+		webDataBinder.registerCustomEditor(String.class, "studentName", new StudentNameEditor());
+		
+	}
+	
+	
+
+	@RequestMapping(value = "/reg", method = RequestMethod.GET)
+	public ModelAndView register() {
+
+		ModelAndView modelAndView = new ModelAndView("Registration");
+		return modelAndView;
+	}
+
+	// using @ModelAttribute
+	@RequestMapping(value = "/submitRegistationForm", method = RequestMethod.POST)
+	public ModelAndView submit(@ModelAttribute("student1") Student student , BindingResult bindingResult) {
+
+		if(bindingResult.hasErrors()){
+			
+			ModelAndView modelAndView = new ModelAndView("Registration");
+			return modelAndView;
+		}
+		
+		
+		ModelAndView modelAndView = new ModelAndView("success");
+
+		return modelAndView;
+
+	}
+}
